bd629a299439e7b521ae3b08ecd6c2d218b61bf3
cheefs <cheefst@outlook.com>
2020-05-17 15:23:52 +0700

    [*] добавил monolog, прологировал точку входа, и поставил профилировщик на ней, чтоб посмотреть trace того как грузятся страници ( данный лог пришлось урезать, изза шаблонизатора и прочих зависимостей он был 350 мб )

--- practice2/public/index.php
+++ practice2/public/index.php
@@ -7,12 +7,17 @@ include TRANSLATE;
 use app\services\TwigRenderService;
 use app\services\Request;
 use app\services\Session;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$timeStart = microtime();
+$memoryUsageStart = memory_get_usage();
 
 $request = new Request();
 $session = new Session([
     Session::USER_ID => 1, // Так как авторизация не реализована, а для работы корзины нужен пользоатель, по умолчанию зададим id
 ]);
-
+xdebug_start_trace();
 $controller = $request->getControllerName()?: 'default';
 $action = $request->getActionName()?: 'index';
 
@@ -24,3 +29,21 @@ if (class_exists($controllerClass)) {
 } else {
     header('Location: /');
 }
+
+$timeEnd = microtime();
+
+$log = new Logger('app');
+$log->pushHandler(new StreamHandler('log/app.log', Logger::DEBUG ));
+$log->debug("app start",  [
+    sprintf( "time spent: %s milliseconds", $timeStart - $timeEnd )
+]);
+
+$memoryUsageEnd = memory_get_usage();
+
+$log->debug("memory usage",  [
+    "before launch: $memoryUsageStart",
+    "after: $memoryUsageEnd",
+    sprintf('diff: %s', $memoryUsageEnd - $memoryUsageStart )
+]);
+
+xdebug_stop_trace();
